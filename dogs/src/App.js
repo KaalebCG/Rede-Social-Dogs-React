c0b14c4e11251f52c8d8a25944c3e3ecdd0093d0
KaalebCD <kaleb.damaceno@gmail.com>
2023-02-13 12:49:13 -0300

    first commit

---
+++ dogs/src/App.js
@@ -0,0 +1,23 @@
+import Header from "./Components/Header/Header";
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Components/Login/Login";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
