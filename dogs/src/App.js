e2fa0dfeb473f04589853e0c89f039c0a607d446
KaalebCD <kaleb.damaceno@gmail.com>
2023-02-16 15:22:40 -0300

    adicionando api de teste e usercontext

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -4,17 +4,20 @@ import Footer from "./Components/Footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Components/Login/Login";
+import { UserStorage } from "./Context/UserContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
