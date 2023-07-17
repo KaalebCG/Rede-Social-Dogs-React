11d4bd6fa1a4e37e5bbaf3d712ebcd4058397b9d
KaalebCD <kaleb.damaceno@gmail.com>
2023-07-17 10:42:22 -0300

    `feat:` adicionando validaçoes no formulario

--- src/pages/Login/LoginForm.jsx
+++ src/pages/Login/LoginForm.jsx
@@ -3,25 +3,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../../Components/Forms/Input";
 import Button from "../../Components/Forms/Button";
+import { useForm } from "../../Hooks/useForm";
 
 export const LoginForm = () => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
-    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    })
-      .then((r) => {
-        console.log(r);
-        return r.json();
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => console.log(json));
+        .then((r) => {
+          console.log(r);
+          return r.json();
+        })
+        .then((json) => console.log(json));
+    }
   }
 
   return (
@@ -29,8 +33,8 @@ export const LoginForm = () => {
       LoginForm
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Entrar</Button>
       </form>
