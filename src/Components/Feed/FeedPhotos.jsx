7696746eaed687bba09adc1a8a4517b1778421a8
KaalebCD <kaleb.damaceno@gmail.com>
2023-07-25 15:02:37 -0300

    `feat:` adicionando func de modal de fotos

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,12 +6,12 @@ import { Error } from "../Helper/Error";
 import { Loading } from "../Helper/Loading";
 import styles from "./FeedPhotos.module.css";
 
-export const FeedPhotos = () => {
+export const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 30, user: 0 });
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
       const { json } = await request(url, options);
       console.log(json);
     }
@@ -24,7 +24,11 @@ export const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map((photo) => (
-          <FeedPhotosItem key={photo.id} photo={photo} />
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
         ))}
       </ul>
     );
