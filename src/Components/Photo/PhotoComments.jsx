02e5c00c0cfba61459ff4c8951cabe63d4b65556
KaalebCD <kaleb.damaceno@gmail.com>
2023-07-27 12:06:02 -0300

    `feat:` adicionando func de `comentarios`

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { UserContext } from "../../UserContext/UserContext";
+import { PhotoCommentsForm } from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
 
-export const PhotoComments = () => {
+export const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
   return (
-    <div>PhotoComments</div>
-  )
-}
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
+  );
+};
